0013c44d47ef008c5e7f6ac1d7154ba9caf8aa48
Amani Jumaloon <amani.jumaloon966@gmail.com>
2026-03-01 06:14:58 +0500

    Week 2 Step 2: created database with plaintext credentials

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const Database = require("better-sqlite3");
+const path = require('path');
+
 const app = express();
 const port = 3000;
+
+// Database setup
+const db = new Database("database.db");
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+  )
+`);
+
+// Seeding a test user with plain text password and no hashing
+const existing = db.prepare("SELECT * FROM users WHERE username = 'admin'").get();
+if (!existing) {
+  db.prepare("INSERT INTO users (username, password) VALUES (?, ?)").run("admin", "password123");
+}
 
 // App (server) handles incoming GET requests to the root URL (/)
 // (The browser sends requests)
@@ -8,7 +28,6 @@ app.get('/', (req, res) => {
   res.send('<h1>HackShield<h1>');
 });
 
-const path = require('path');
 // Lets express read form data
 app.use(express.urlencoded({extended:true}));
 
@@ -16,6 +35,7 @@ app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, "views", "login.html"));
 });
 
+// The browser sends inputted form data to the server through a POST request
 app.post('/login', (req, res) => {
     const {username, password} = req.body;
     res.send('Received: ${username} / ${password}');
